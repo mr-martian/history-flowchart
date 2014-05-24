869035929ce6a74140bad10b91165c6717a15daa
mr-martian <popcorn.tomato.dude@gmail.com>
2014-05-24 11:39:17 -0500

    Create get_graph.php

---
+++ code/interface/get_graph.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    <?php
+      //using sockets get $result
+      $result = 5;
+      if (is_string($result){
+        print($result);}
+      else{
+        echo "An error has occured, please try again."; //add details
+      }
+    ?>
+  </body>
+</html>
