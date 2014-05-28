ac54ec1ad1591d48d255e539ed7b4f0a90845eb9
mr-martian <popcorn.tomato.dude@gmail.com>
2014-05-28 07:44:32 -0500

    Update graph.lisp

--- code/graph.lisp
+++ code/graph.lisp
@@ -10,12 +10,16 @@
     (car event))
 (defun get-link (event)
 	(format nil "historyflowchart.com/essays/event.php?u=~a, e=~a" (get-name event))) ;change if necessary
+(defun get-color (event)
+	'red)
 (defun points-svg (events)
     (loop for ev in events collecting
         (format nil "<circle r=\"5\" stroke=\"black\" stroke-width=\"1\" cx=\"~d\" cy=\"~d\" fill=\"~a\" onclick=\"window.open('~a')\"><title>~a</title></circle>"
     		(car (get-coords ev)) (cadr (get-coords ev))
             (get-color ev) ;based on type of event?
             (get-link event) (get-name ev))))
+(defun lines-svg (effects)
+	(loop for ef in effects collecting (list (car ef) (cadr ef))))
 (defun make-graph (events effects)
     (let ((gr (points-svg events)))
     	nil))
