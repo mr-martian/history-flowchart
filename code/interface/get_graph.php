1aa8dda53b88ccc0b2a4c6bd3a9f27e41c198c5b
mr-martian <popcorn.tomato.dude@gmail.com>
2014-05-28 20:44:09 -0500

    Update get_graph.php

--- code/interface/get_graph.php
+++ code/interface/get_graph.php
@@ -3,6 +3,7 @@
     <?php
       //add more stuff, directions, etc.
       $file = tempnam("blah/blah/graphs", "graph116"); //make better names somehow, time maybe
+      chmod($file, 0777);
       exec(join(" ", array("graph.exe", $file, $_GET['event'], $_GET['start'], $_GET['end'])));
       $graph = fopen($file, "r") or die("Unable to open file!");
       echo fgets($graph);
