27ccb5b0ed5b4844a5e03963f2d62c31129ae94d
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-01 19:05:20 -0500

    Update get_level.php

--- code/interface/get_level.php
+++ code/interface/get_level.php
@@ -6,7 +6,7 @@
       include 'universe_list.php';
       $lv=$UNIVERSES[$_GET['u']][2];
       for($x=0;$x<count($lv);$x++){
-        echo '<br /><a href="gg3.php?u=', $_GET['u'], '&lv=', $x+1 '&', htmlspecialchars(SID) '">', $x+1, ': ', $lv[$x], '</a>';
+        echo '<br /><a href="get_event.php?u=', $_GET['u'], '&lv=', $x+1 '&', htmlspecialchars(SID) '">', $x+1, ': ', $lv[$x], '</a>';
       }
     ?>
   </body>
