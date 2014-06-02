9a6a4591e44e89ba29356059c810652fdf7104fc
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-01 19:29:08 -0500

    Update and rename event_list.html to get_event.php

---
+++ code/interface/get_event.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<html>
+  <body>
+    <H2>Enter Event Name</H2>
+    <p>Enter the name of the event you want the graph centered around.</p>
+    <p>If you enter nothing, or something that the graph maker does not recognize, it will assume you mean everything</p>
+    <form method="get" action=<?php echo "get_date.php?u=", $_GET['u'], '&lv=', $_GET['lv'], '&', htmlspecialchars(SID) ?>>
+      Event: <input type="text" name="e">
+      <input type="submit" />
+    </form>
+  </body>
+</html>
