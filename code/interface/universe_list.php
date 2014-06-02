0ecf18acef741414cdbc47a9b37cfb8e84ef8292
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-01 19:04:15 -0500

    Create universe_list.php

---
+++ code/interface/universe_list.php
@@ -0,0 +1,6 @@
+<?php
+  $UNIVERSES = array( //array(name, description, levels) levels = array(level_1_description, level_2_description, ...)
+    array("Human", "Recorded human history", array("level 1", "level 2", "level 3", "level 4", "level 5")),
+    array("Natural", "History of the universe prior to the advent of human civilization", array("level 1", "level 2"))
+  );
+?>
