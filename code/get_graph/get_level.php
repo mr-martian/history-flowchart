ced9fdfad453124a3a11adf65364ddfd9e217af7
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-07 09:46:56 -0500

    Update get_level.php

--- code/get_graph/get_level.php
+++ code/get_graph/get_level.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <html>
   <body>
     <h3>Select Level</h3>
@@ -6,7 +5,7 @@
       include 'universe_list.php';
       $lv=$UNIVERSES[$_GET['u']][2];
       for($x=0;$x<count($lv);$x++){
-        echo '<br /><a href="get_event.php?u=', $_GET['u'], '&lv=', $x+1 '&', htmlspecialchars(SID) '">', $x+1, ': ', $lv[$x], '</a>';
+        echo '<br /><a href="get_event.php?u=', $_GET['u'], '&lv=', $x+1, '">', $x+1, ': ', $lv[$x], '</a>';
       }
     ?>
   </body>
