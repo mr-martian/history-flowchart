8a27b5adb5d97ca2db3055b4c69a86416c661ef7
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-07 10:31:24 -0500

    Update get_graph.php

--- code/get_graph/get_graph.php
+++ code/get_graph/get_graph.php
@@ -1,14 +1,19 @@
-<?php session_start(); ?>
 <html>
   <body>
     <?php
-      //add more stuff, directions, etc.
-      $file = tempnam("blah/blah/graphs", session_name());
-      chmod($file, 0777);
-      exec(join(" ", array("graph.exe", $file, $_GET['u'], $_GET['lv'], $_GET['e'])));
-      $graph = fopen($file, "r") or die("Unable to open file!");
-      echo fgets($graph);
-      fclose($graph);
+      include 'universe_list.php';
+      include 'path/to/data/' . $UNIVERSES[$_GET['u']][0] . '/settings.php';
+      function ev2svg($event) {
+        $s0 = '<circle r="5" stroke="black" stroke-width="1" cx="';
+        $s1 = '" cy="';
+        $s2 = '" fill="';
+        $s3 = '" onclick="window.open(\'';
+        $s4 = '\')"><title>';
+        $s5 = '</title></circle>';
+        $rs0 = $s0 . get_time_coord($event) . $s1 . get_space_coord($event) . $s2;
+        $rs1 = get_color($event) . $s3 . 'path/to/get_essay.php?u=' . $UNIVERSES[$_GET['u']][0] . '&e=' . $event[0];
+        return $rs0 . $rs1 . $s4 . $event[0] . $s5;
+      }
     ?>
   </body>
 </html>
