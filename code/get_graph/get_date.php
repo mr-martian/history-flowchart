c7fc3b3ecc08b8849661bf2418845aae4a9c5f7a
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-07 09:45:58 -0500

    Update get_date.php

--- code/get_graph/get_date.php
+++ code/get_graph/get_date.php
@@ -3,7 +3,7 @@
     <h3>Select Date</h3>
     <?php
       echo 'Sorry, we haven\'t implemented date selection yet. Click ';
-      echo '<a href="get_graph.php?u=', $_GET['u'], '&lv=', $_GET['lv'], '&e=', $_GET['e'], '&', htmlspecialchars(SID), '">here</a>';
+      echo '<a href="get_graph.php?u=', $_GET['u'], '&lv=', $_GET['lv'], '&e=', $_GET['e'], '">here</a>';
       echo ' to continue.';
     ?>
   </body>
