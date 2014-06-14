ef499d76fedd76f59e77ce25178d07263eb1de86
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-14 13:28:43 -0500

    Update get_date.php

--- code/get_graph/get_date.php
+++ code/get_graph/get_date.php
@@ -2,9 +2,16 @@
   <body>
     <h3>Select Date</h3>
     <?php
-      echo 'Sorry, we haven\'t implemented date selection yet. Click ';
-      echo '<a href="get_graph.php?u=', $_GET['u'], '&lv=', $_GET['lv'], '&e=', $_GET['e'], '">here</a>';
-      echo ' to continue.';
+      echo '<form method="get" action="get_graph.php?u=', $_GET['u'], '&lv=', $_GET['lv'], '&e=', $_GET['e'], '">';
+      echo 'Begin in <input type="number" name="start">';
+      echo '<input type="radio" name="sbc" value="bc">BC';
+      echo '<input type="radio" name="sbc" value="ad">AD<br>';
+      //consider sending date of event to be initial contents of input box
+      echo 'End in <input type="number" name="end">';
+      echo '<input type="radio" name="ebc" value="bc">BC';
+      echo '<input type="radio" name="ebc" value="ad">AD<br>';
+      echo '<input type="submit">';
+      echo '</form>';
     ?>
   </body>
 </html>
