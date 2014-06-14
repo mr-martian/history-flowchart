1b3c404fb1c2d2dc20c2e97ae235c06acd1abdbd
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-14 15:01:11 -0500

    Create setup.php

---
+++ code/setup.php
@@ -0,0 +1,35 @@
+<?php
+  $con=mysqli_connect("flowcharthistory.com", "user", "password");
+  // Check connection
+  if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  // Create database
+  $sql="CREATE DATABASE flowchart_data";
+  if (mysqli_query($con,$sql)) {
+    echo "Database flowchart_data created successfully";
+  } else {
+    echo "Error creating database: " . mysqli_error($con);
+  }
+  
+  // Create table
+  $sql="CREATE TABLE Events(PID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(PID), Name CHAR(30), Universe CHAR(10), Level SMALLINT, Date CHAR(15), Location CHAR(15))";
+
+  // Execute query
+  if (mysqli_query($con,$sql)) {
+    echo "Table Events created successfully";
+  } else {
+    echo "Error creating table: " . mysqli_error($con);
+  }
+    
+  // Create table
+  $sql="CREATE TABLE Effects(PID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(PID), From CHAR(30), To CHAR(30), Type CHAR(15))";
+
+  // Execute query
+  if (mysqli_query($con,$sql)) {
+    echo "Table Effects created successfully";
+  } else {
+    echo "Error creating table: " . mysqli_error($con);
+  }
+?>
