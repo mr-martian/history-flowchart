4747d6633cdc4b8d410bd31dc6af6b1b05fa39ed
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-14 16:09:48 -0500

    Update display_event.php

--- code/essay/display_event.php
+++ code/essay/display_event.php
@@ -1,4 +1,9 @@
 <?php
   $path = "essays/events/" . $_GET['n'] . "/";
   $list = scandir($path);
+  foreach ($list as $fp){
+    $file = fopen($fp, "r");
+    echo fgets($file);
+    echo "<hr>";
+  }
 ?>
