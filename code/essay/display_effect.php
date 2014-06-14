d229c6d93233867f12fa4697651e60b6ae532371
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-14 16:23:16 -0500

    Create display_effect.php

---
+++ code/essay/display_effect.php
@@ -0,0 +1,9 @@
+<?php
+  $path = "essays/effects/" . $_GET['n'] . "/";
+  $list = scandir($path);
+  foreach ($list as $fp){
+    $file = fopen($fp, "r");
+    echo fgets($file);
+    echo "<hr>";
+  }
+?>
