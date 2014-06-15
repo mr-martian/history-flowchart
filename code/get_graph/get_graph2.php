1f69d213741d0cc603001fa34341ce8768e24819
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-15 16:08:35 -0500

    Update and rename get_graph.html to get_graph2.php

--- code/get_graph/get_graph2.php
+++ code/get_graph/get_graph2.php
@@ -1,6 +1,12 @@
 <html>
   <body>
-    <iframe src="show_universes.php?target=lframe" seamless></iframe>
+    <?php
+      include "universe_list.php";
+      foreach($UNIVERSES as $u){
+        echo "<a href='show_levels.php?u=", $u[0], "' target='lframe'>", $u[0], "</a>";
+      }
+    ?>
+    <br>
     <iframe src="show_levels.php?u=Human&target=eframe" name="lframe"></iframe>
     <iframe src="show_events.php?u=Human&target=_parent" name="eframe"></iframe>
   </body>
