ee285ba10318334510c2ca6e9288e0ee5e725d49
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-15 16:17:06 -0500

    Update get_graph2.php

--- code/get_graph/get_graph2.php
+++ code/get_graph/get_graph2.php
@@ -6,7 +6,7 @@
         echo "<a href='show_levels.php?u=", $u[0], "' target='lframe'>", $u[0], ": ", $u[1], "</a><br>";
       };
     ?>
-    <iframe src="show_levels.php?u=Human&target=eframe" name="lframe"></iframe>
-    <iframe src="show_events.php?u=Human&target=_parent" name="eframe"></iframe>
+    <iframe src="show_levels.php?u=Human" name="lframe"></iframe>
+    <iframe src="show_events.php?u=Human&level=1" name="eframe"></iframe>
   </body>
 </html>
