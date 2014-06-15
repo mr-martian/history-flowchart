8582fcb948fd0f1c25afe6223ab92bd1d7293d8f
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-15 16:11:18 -0500

    Update get_graph2.php

--- code/get_graph/get_graph2.php
+++ code/get_graph/get_graph2.php
@@ -3,10 +3,9 @@
     <?php
       include "universe_list.php";
       foreach($UNIVERSES as $u){
-        echo "<a href='show_levels.php?u=", $u[0], "' target='lframe'>", $u[0], "</a>";
+        echo "<a href='show_levels.php?u=", $u[0], "' target='lframe'>", $u[0], ": ", $u[1], "</a><br>";
       }
     ?>
-    <br>
     <iframe src="show_levels.php?u=Human&target=eframe" name="lframe"></iframe>
     <iframe src="show_events.php?u=Human&target=_parent" name="eframe"></iframe>
   </body>
