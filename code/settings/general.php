d82e0e26c388316f780bda17c37157e206134e08
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-17 14:17:50 -0500

    Create general.php

---
+++ code/settings/general.php
@@ -0,0 +1,19 @@
+<?php
+  $UNIVERSES = array( //array(name, description, levels) levels = array(level_1_description, level_2_description, ...)
+    array("Human", "Recorded human history", array("level 1", "level 2", "level 3", "level 4", "level 5")),
+    array("Natural", "History of the universe prior to the advent of human civilization", array("level 1", "level 2"))
+  );
+  function is_valid_name($name) {
+    return ctype_alnum(str_replace(' ', '', $name)) and strlen($name) < 30;
+  }
+  function connect() {
+    $con=mysqli_connect("example.com","peter","abc123","my_db");
+    // Check connection
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    else {
+      return $con;
+    }
+  }
+?>
