0888e7bc2ab34d04278583aba4f390cc767d5f5a
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-28 10:21:47 -0500

    Update natural.php

--- settings/natural.php
+++ settings/natural.php
@@ -1,6 +1,7 @@
 <?php
   $places = array('universe' => 5,
                   'milky way' => 10);
+  $types = array('evolution' => 'blue'); //must be less than 15 chars
   function is_valid_date($string) {
     $n = sscanf($string, "%d");
     return $string == sprintf("%d", $n);
@@ -8,8 +9,11 @@
   function is_valid_place($string) {
     array_key_exists($string, $places);
   }
+  function is_valid_type($string) {
+    array_key_exists($string, $types);
+  }
   function get_effect_color($effect) {
-    return 'green';
+    return $types[$effect['type']];
   }
   function get_event_color($event) {
     return 'red';
