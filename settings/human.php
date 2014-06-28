4a844b2f28129f2b1016b369290b0ac97978152f
mr-martian <popcorn.tomato.dude@gmail.com>
2014-06-28 10:21:39 -0500

    Update human.php

--- settings/human.php
+++ settings/human.php
@@ -1,4 +1,6 @@
 <?php
+  $types = array('political' => 'red',
+                 'ideological' => 'blue'); //must be less than 15 chars
   function is_valid_date($string) {
     $s = sscanf($string, "%d");
     return $string == sprintf("%d", $s[0]);
@@ -7,8 +9,11 @@
     $s = sscanf($string, "(%d, %d)");
     return $string == sprintf("(%d, %d)", $s[0], $s[1]);
   }
+  function is_valid_type($string) {
+    return array_key_exists($string, $types);
+  }
   function get_effect_color($effect) {
-    return 'green';
+    return $types[$effect['type']];
   }
   function get_event_color($event) {
     return 'red';
