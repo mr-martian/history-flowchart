6134241b2f5d2c9ba31eed185837134ac6a886ec
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-01 11:29:41 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -22,10 +22,10 @@
     $int = mysqli_fetch_array(mysqli_query("SELECT * FROM Events WHERE Universe = " . $_POST['universe'] . " and Name = " . $_POST['name']))[0]['PID'];
     $file = fopen("essays/events/" . $int . ".html", "x");
     if ($file) {
-      fwrite($file, "<html><body><a href='mailto:flowcharthistory@googlegroups.com?Subject=" . $_POST['from'] . "+to+" . $_POST['to'] . "'>Report this essay</a>");
+      fwrite($file, "<html><body><a href='mailto:flowcharthistory@googlegroups.com?Subject=" . $_POST['from'] . "+to+" . $_POST['to'] . "'>Discuss this essay</a>");
       fwrite($file, "<br><h1>" . $_POST['from'] . ' to ' . $_POST['to'] . "</h1><br><p>Universe: " . $_POST['universe'] . "</p>");
       fwrite($file, "<p>Type: " . $_POST['type'] . "</p><pre>" . htmlspecialchars($_POST['essay']) . "</pre>");
-      fwrite($file, "<p><b>Sources</b></p><pre>" . htmlspecialchars($_POST['sources]) . "</pre></body></html>");
+      fwrite($file, "<p><b>Sources</b></p><pre>" . htmlspecialchars($_POST['sources']) . "</pre></body></html>");
       echo "<h1>Record Added Successfully!</h1><br><p>Please close this window.</p>";
     }
   }
