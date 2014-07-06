5fb9706e2f162de03141b69aca53a676cd3afffd
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-06 17:37:28 -0500

    Update request_graph.php
    remove levels

--- get_graph/request_graph.php
+++ get_graph/request_graph.php
@@ -2,11 +2,10 @@
   <body>
     <?php
       include "settings/general.php";
-      foreach($UNIVERSES as $u){
-        echo "<a href='show_levels.php?u=", $u[0], "' target='lframe'>", $u[0], ": ", $u[1], "</a><br>";
+      foreach(array_keys($UNIVERSES) as $u){
+        echo "<a href='show_levels.php?u=", $u, "' target='eframe'>", $u, ": ", $UNIVERSES[$u], "</a><br>";
       };
     ?>
-    <iframe src="show_levels.php?u=Human" name="lframe"></iframe>
-    <iframe src="show_events.php?u=Human&level=1" name="eframe"></iframe>
+    <iframe src="show_events.php?u=Human" name="eframe"></iframe>
   </body>
 </html>
