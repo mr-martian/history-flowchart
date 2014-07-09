ddb65cffe4b11fb1bef7dfc9437f3e8f31cade78
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 15:01:34 -0500

    Create submit_event_essay.php

---
+++ submit/submit_event_essay.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <form action="handle_event_essay.php" method="POST">
+      <textarea rows="100" cols="80" required name="essay">Please herein describe the event and its importance.</textarea>
+      <textarea rows="15" cols="80" required name="sources">Sources go here!</textarea>
+    </form>
+  </body>
+</html>
