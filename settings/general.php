590c4b7d9dcad815ff595eb5ee60bdfb89aaa0cf
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 18:45:08 -0500

    Update general.php

--- settings/general.php
+++ settings/general.php
@@ -3,6 +3,11 @@
     "Human" => "Recorded human history",
     "Natural" => "History of the universe prior to the advent of human civilization"
   );
+  function get_universe($name) {
+    if array_key_exists($name, $UNIVERSES) {
+      include "settings/".$name.".php";
+    }
+  }
   function is_valid_name($name) {
     return ctype_alnum(str_replace(' ', '', $name)) and strlen($name) < 30;
   }
