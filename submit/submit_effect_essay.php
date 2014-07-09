f214de5ea0af28b4a5e8f193d0bd5615b571dcb8
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 16:25:43 -0500

    Update submit_effect_essay.php

--- submit/submit_effect_essay.php
+++ submit/submit_effect_essay.php
@@ -5,7 +5,7 @@
       $con = connect();
       $ef = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM Effects WHERE PID = " . $_GET['id']))[0];
       echo "<h1>", $ef['Cause'], " to ", $ef['Effect'], "</h1>";
-      echo "<form action='handle_event_essay.php'" . $_GET['id'] . " method='POST'>";
+      echo "<form action='handle_effect_essay.php?" . $_GET['id'] . "' method='POST'>";
       echo "<input type='text' maxlength='50' name='title'>Title</input>"
       echo "<textarea rows='100' cols='80' required name='essay'>Please herein describe the details of how";
       echo $ef['Cause'], " led to ", $ef['Effect'], ".</textarea>";
