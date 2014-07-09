5a9f12985a7dc6f901b03cda70e37c7d6771d1bd
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 18:42:56 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -13,12 +13,12 @@
   elseif(!is_valid_type($_POST['type'])) { 
     echo "The 'Type' you entered is invalid, please only use letters, numbers, and spaces."; 
   }
-  elseif(mysqli_fetch_array(mysqli_query($con,"SELECT * FROM Effects WHERE Cause = " . sqlite_escape_string($_POST['from']) . " and Effect = " . sqlite_escape_string($_POST['to']) . " and Universe = " . $_POST['universe']))){
+  elseif(mysqli_fetch_array(mysqli_query($con,"SELECT * FROM Effects WHERE Cause = " . sqlite_escape_string($_POST['from']) . " and Effect = " . sqlite_escape_string($_POST['to']) . " and Type = " . $_POST['type']))){
     echo 'Sorry, that cause & effect chain has already been entered. Please add something new.';
   }
   else {
-    $str = $_POST['from'] . "', '" . $_POST['to'] . "', '" . $_POST['universe'] . "', '" . $_POST['type'] . "')"
-    mysqli_query($con,"INSERT INTO Events (Cause, Effect, Universe, Type) VALUES ('" . $str);
+    $str = $_POST['from'] . "', '" . $_POST['to'] . "', '" . $_POST['type'] . "')"
+    mysqli_query($con,"INSERT INTO Events (Cause, Effect, Type) VALUES ('" . $str);
     $int = mysqli_fetch_array(mysqli_query("SELECT * FROM Events WHERE  Cause = " . $_POST['from'] . " and Effect = " . $_POST['to'] . " and Universe = " . $_POST['universe']))[0]['PID'];
     echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_effect_essay.php?id=" . $int . "'>here</a> to add an extended description.</p>";
   }
