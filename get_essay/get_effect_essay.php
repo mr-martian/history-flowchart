c212cc3e7998fe11d67a4db6373bad927eff27dd
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 18:13:09 -0500

    Create get_effect_essay.php

---
+++ get_essay/get_effect_essay.php
@@ -0,0 +1,13 @@
+<?php
+  include "settings/general.php";
+  $con = connect();
+  $ev = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM Effects WHERE PID = " . $_GET['id']));
+  $es = mysqli_query($con,"SELECT * FROM Fessays WHERE About = " . $_GET['id']);
+  echo "<h1>", $ev['Name'], "</h1>";
+
+while($row = mysqli_fetch_array($result)) {
+  echo "<br><a href='display_effect_essay.php?id=", $row['PID'], "'>", $row['Title'], "</a>";
+}
+echo "<br>";
+echo "<a href='submit/submit_effect_essay?id=", $_GET['id'], "'>Submit your own</a>";
+?>
