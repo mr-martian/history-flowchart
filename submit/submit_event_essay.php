2e7fac379ab6629c608163698bbf501a46e1962b
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 15:10:44 -0500

    Update submit_event_essay.php

--- submit/submit_event_essay.php
+++ submit/submit_event_essay.php
@@ -1,8 +1,10 @@
 <html>
   <body>
-    <form action="handle_event_essay.php" method="POST">
-      <textarea rows="100" cols="80" required name="essay">Please herein describe the event and its importance.</textarea>
-      <textarea rows="15" cols="80" required name="sources">Sources go here!</textarea>
-    </form>
+    <?php
+      echo "<form action='handle_event_essay.php'" . $_GET['id'] . " method='POST'>";
+      echo "<textarea rows='100' cols='80' required name='essay'>Please herein describe the event and its importance.</textarea>";
+      echo "<textarea rows='15' cols='80' required name='sources'>Sources go here!</textarea>";
+      echo "</form>";
+    ?>
   </body>
 </html>
