bc2fc52221b4e35e076e971bf530220d2f6e79a6
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-09 15:06:04 -0500

    Update handle_event.php

--- submit/handle_event.php
+++ submit/handle_event.php
@@ -20,13 +20,6 @@
     $str = $_POST['name'] . "', '" . $_POST['universe'] . "', '" . $_POST['date'] . "', '" . $_POST['location'] . ")"
     mysqli_query($con,"INSERT INTO Events (Name, Universe, Date, Location) VALUES ('" . $str);
     $int = mysqli_fetch_array(mysqli_query("SELECT * FROM Events WHERE Universe = " . $_POST['universe'] . " and Name = " . $_POST['name']))[0]['PID'];
-    $file = fopen("essays/events/" . $int . ".html", "x");
-    if ($file) {
-      fwrite($file, "<html><body><a href='mailto:flowcharthistory@googlegroups.com?Subject=" . $_POST['name'] . "'>Report this essay</a>");
-      fwrite($file, "<br><h1>" . $_POST['name'] . "</h1><br><p>Universe: " . $_POST['universe'] . "</p>");
-      fwrite($file, "<p>Date: " . $_POST['date'] . "</p><p>Location: " . $_POST['location'] . "</p><pre>" . htmlspecialchars($_POST['essay']) . "</pre>");
-      fwrite($file, "<p><b>Sources</b></p><pre>" . htmlspecialchars($_POST['sources']) . "</pre></body></html>");
-      echo "<h1>Record Added Successfully!</h1><br><p>Please close this window.</p>";
-    }
+    echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_event_essay.php?id=" . $int . "'>here</a> to add an extended description.</p>";
   }
 ?>
