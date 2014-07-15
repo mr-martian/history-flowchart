12d203a3e985cf239494f105a26bdfa9ff7e3ea5
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-15 15:31:46 -0500

    Create handle_essay.php

---
+++ submit/handle_essay.php
@@ -0,0 +1,17 @@
+<html>
+  <body>
+    <?php
+      include "settings/general.php";
+      $con = connect();
+      $w = ($_GET['type'] == 'v' ? 'Vessays' : 'Fessays');
+      mysqli_query($con,"INSERT INTO " . $w . " (Title, About) VALUES ('" . $_POST['title'] . "', " . $_GET['id'] . ")");
+      $int = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM " . $w . " WHERE Title = " . $_POST['title'] . " and About = " . $_GET['id']))[0]['PID'];
+      $file = fopen("essays/" . ($_GET['type'] == 'v' ? 'events' : 'effects') . "/" . $int . ".txt", "x");
+      if ($file) {
+        fwrite($file, htmlspecialchars($_POST['essay']) . "\n\n");
+        fwrite($file, "<h3>SOURCES</h3>" . htmlspecialchars($_POST['sources']));
+        echo "record succesfully added.";
+      }
+    ?>
+  </body>
+</html>
