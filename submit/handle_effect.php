22adc05cb0d893a5d5a7d68b94395397508e9559
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-16 15:40:07 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -5,10 +5,13 @@
   if(get_effect($cause=$_GET['c'], $effect=$_GET['e'], $type=$_GET['t'], $array=true)){
     echo 'Sorry, that cause & effect chain has already been entered. Please add something new.';
   }
+  elseif(!is_valid_type($_GET['t'])) {
+    echo 'Type invalid, please try resubmitting the form.'
+  }
   else {
-    $str = $_POST['c'] . ", " . $_POST['e'] . ", '" . $_POST['t'] . "', '" . $_POST['u'] . "')"
+    $str = intval($_GET['c']) . ", " . intval($_GET['e']) . ", '" . $_GET['t'] . "', '" . $_GET['u'] . "')"
     mysqli_query($con,"INSERT INTO Events (Cause, Effect, Type, Universe) VALUES (" . $str);
-    $int = get_effect($cause=$_GET['c'], $effect=$_GET['e'], $universe =$_GET['u'], $type=$_GET['t'], $array=true)['PID'];
+    $int = get_effect($cause=intval($_GET['c']), $effect=intval($_GET['e']), $universe =$_GET['u'], $type=$_GET['t'], $array=true)['PID'];
     echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=f&id=" . $int . "' target='submit'>here</a> to add an extended description.</p>";
   }
 ?>
