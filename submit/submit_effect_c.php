a58693e58e1cfc202a46cf241f29d35695338cf3
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-16 14:42:02 -0500

    Update submit_effect_c.php

--- submit/submit_effect_c.php
+++ submit/submit_effect_c.php
@@ -1,7 +1,6 @@
 <?php
   include "settings/general.php";
-  $con = connect();
-  $result = mysqli_query($con, "SELECT * FROM Events WHERE Universe = " . $_GET['u']);
+  $result = get_event($universe=$_GET['u']);
   while($row = mysqli_fetch_array($result)) {
     echo "<br><a href='submit_effect_e.php?u=", $_GET['u'], "&c=", $row['PID'], "' target='effect'>", $row['Name'], "</a>";
   }
