4579417ba4173a8e1218c76b9c7e8a15d9c6db93
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-27 14:21:03 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -11,8 +11,12 @@
   else {
     $con = connect();
     $str = intval($_GET['c']) . ", " . intval($_GET['e']) . ", '" . $_GET['t'] . "', '" . $_GET['u'] . "')";
-    mysqli_query($con,"INSERT INTO Events (Cause, Effect, Type, Universe) VALUES (" . $str);
-    $int = mysqli_fetch_array(get_effect($cause=intval($_GET['c']), $effect=intval($_GET['e']), $universe =$_GET['u'], $type=$_GET['t']));
-    echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=f&id=" . $int['PID'] . "' target='submit'>here</a> to add an extended description.</p>";
+    if (mysqli_query($con,"INSERT INTO Events (Cause, Effect, Type, Universe) VALUES (" . $str)) {
+      $int = get_effect($cause=intval($_GET['c']), $effect=intval($_GET['e']), $universe =$_GET['u'], $type=$_GET['t'], $array=true);
+      echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=f&id=" . $int['PID'] . "' target='submit'>here</a> to add an extended description.</p>\n";
+    }
+    else {
+      echo "<h1>There was an error, please try again.</h1>";
+    }
   }
 ?>
