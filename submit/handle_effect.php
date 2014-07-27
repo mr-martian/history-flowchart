684946f325a25211f9124cf85c336a4a922a4c32
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-27 14:32:31 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -10,7 +10,7 @@
   }
   else {
     $con = connect();
-    $str = intval($_GET['c']) . ", " . intval($_GET['e']) . ", '" . $_GET['t'] . "', '" . $_GET['u'] . "')";
+    $str = intval($_GET['c']) . ", " . intval($_GET['e']) . ", '" . $_GET['t'] . "', '" . $_GET['u'] . "');";
     if (mysqli_query($con,"INSERT INTO Events (Cause, Effect, Type, Universe) VALUES (" . $str)) {
       $int = get_effect($cause=intval($_GET['c']), $effect=intval($_GET['e']), $universe =$_GET['u'], $type=$_GET['t'], $array=true);
       echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=f&id=" . $int['PID'] . "' target='submit'>here</a> to add an extended description.</p>\n";
