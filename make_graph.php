642396375b34f01863e608c13cd75cd7e98ed5ed
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-27 15:13:20 -0500

    Update make_graph.php

--- make_graph.php
+++ make_graph.php
@@ -73,14 +73,14 @@
       $stuff = get_vfs();
       echo '<svg>';
       echo '<marker xmlns="http://www.w3.org/2000/svg" id="triangle" viewBox="0 0 10 10" refX="0" refY="5" markerUnits="strokeWidth" markerWidth="4" markerHeight="3" orient="auto">
-      <path d="M 0 0 L 10 5 L 0 10 z"/></marker>'
+      <path d="M 0 0 L 10 5 L 0 10 z"/></marker>';
       foreach ($stuff[0] as $ev) {
         echo ev2svg($ev);
       }
       foreach ($stuff[1] as $ef) {
         echo ef2svg($ef);
       }
-      echo '</svg>'
+      echo '</svg>';
     ?>
   </body>
 </html>
