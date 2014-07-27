dd622453b30b49c769ef04a1bfa02f1eb20c332f
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-27 15:16:43 -0500

    Update make_graph.php

--- make_graph.php
+++ make_graph.php
@@ -2,7 +2,7 @@
   <body>
     <?php
       include 'globals.php';
-      include get_universe_path($_GET['u']);
+      include get_universe_path($_GET['u'], $root=true);
 
       function ev2svg($event) {
         $s0 = '<circle r="5" stroke="black" stroke-width="1" cx="';
