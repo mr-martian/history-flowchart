c5450f8aa090267440c142d0645b22bad032b094
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-27 17:18:34 -0500

    Update human.php

--- settings/human.php
+++ settings/human.php
@@ -1,4 +1,5 @@
 <?php
+  $svg = 'height="360" width="7100" viewPort="-5000 -180 2100 180"';
   $types = array('political' => 'red',
                  'ideological' => 'blue'); //must be less than 15 chars
   function is_valid_date($string) {
