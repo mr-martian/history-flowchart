1eff281c348821bf52b499009df66cb6784bcb8a
mr-martian <popcorn.tomato.dude@gmail.com>
2014-07-27 13:35:07 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -6,10 +6,10 @@
     echo 'Sorry, that cause & effect chain has already been entered. Please add something new.';
   }
   elseif(!is_valid_type($_GET['t'])) {
-    echo 'Type invalid, please try resubmitting the form.'
+    echo 'Type invalid, please try resubmitting the form.';
   }
   else {
-    $str = intval($_GET['c']) . ", " . intval($_GET['e']) . ", '" . $_GET['t'] . "', '" . $_GET['u'] . "')"
+    $str = intval($_GET['c']) . ", " . intval($_GET['e']) . ", '" . $_GET['t'] . "', '" . $_GET['u'] . "')";
     mysqli_query($con,"INSERT INTO Events (Cause, Effect, Type, Universe) VALUES (" . $str);
     $int = get_effect($cause=intval($_GET['c']), $effect=intval($_GET['e']), $universe =$_GET['u'], $type=$_GET['t'], $array=true)['PID'];
     echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=f&id=" . $int . "' target='submit'>here</a> to add an extended description.</p>";
