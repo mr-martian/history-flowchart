812ebc4ebe8dc33ca2492b07319de97281f3ab5a
mr-martian <popcorn.tomato.dude@gmail.com>
2015-01-20 08:31:27 -0600

    Update human.php

--- settings/human.php
+++ settings/human.php
@@ -3,7 +3,8 @@
   $types = array('political' => 'red',
                  'ideological' => 'blue'); //must be less than 15 chars
   function is_valid_date($string) {
-    return preg_match("/^-?\d+(\.\d+)?$/", $string) == 1;
+    //return preg_match("/^-?\d+(\.\d+)?$/", $string) == 1;
+    return preg_match("/^\d{1,2}//\d{1,2}//d{1,5}$/", $string) == 1;
   }
   function is_valid_place($string) {
     return preg_match("/^\(-?\d+(\.\d+)?, -?\d+(\.\d+)?\)$/", $string) == 1;
@@ -23,6 +24,19 @@
     return sscanf($event['Location'], "(%f, %f)")[1] + 180;
   }
   function get_time_coord($event) {
-    return sscanf($event['Date'], "%f")[0] + 5000;
+    //return sscanf($event['Date'], "%f")[0] + 5000;
+    sscanf($event['Date'], "%i/%i/%i", $m, $d, $y);
+    $months = array(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31);
+    if ($y % 4 == 0 and ($y % 100 != 0 or $y % 400)) {
+      $months[2] += 1;
+    }
+    for ($i = 1, $i < 12, $i++) {
+      $months[$i] += $months[$i-1];
+    }
+    $yp = $months[$m-1] + $d - 1
+    if ($y < 0) {
+      $y++;
+    }
+    return $y + ($yp/$months[12]);
   }
 ?>
