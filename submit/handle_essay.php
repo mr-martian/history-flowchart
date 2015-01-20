f99787c410afd523df6b46995d86459c313cc251
mr-martian <popcorn.tomato.dude@gmail.com>
2015-01-20 12:41:19 -0600

    Update handle_essay.php

--- submit/handle_essay.php
+++ submit/handle_essay.php
@@ -4,7 +4,7 @@
       include "../globals.php";
       $con = connect();
       $w = ($_GET['type'] == 'v' ? 'Vessays' : 'Fessays');
-      mysqli_query($con,"INSERT INTO " . $w . " (Title, About) VALUES ('" . $_POST['title'] . "', " . $_GET['id'] . ")");
+      mysqli_query($con,"INSERT INTO " . $w . " (Title, About, Name) VALUES ('" . mysqli_real_escape_string($_POST['title']) . "', " . mysqli_real_escape_string($_GET['id']) . "', " . mysqli_real_escape_string($_POST['name']) . ")");
       $int = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM " . $w . " WHERE Title = " . $_POST['title'] . " and About = " . $_GET['id']))[0]['PID'];
       $file = fopen("essays/" . ($_GET['type'] == 'v' ? 'events' : 'effects') . "/" . $int . ".txt", "x");
       if ($file) {
