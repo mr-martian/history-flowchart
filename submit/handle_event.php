856d76e06ac96a9084b4a40e8d3a5eee0a833c96
mr-martian <popcorn.tomato.dude@gmail.com>
2015-01-31 16:27:15 -0600

    Update handle_event.php

--- submit/handle_event.php
+++ submit/handle_event.php
@@ -1,13 +1,12 @@
 <?php
   include "../globals.php";
-  include get_universe_path($_GET['universe']);
   
   $con=connect();
   
   if(!is_valid_name($_GET['name'])) { 
     echo 'The name you entered is invalid, please only use letters, numbers, and spaces.'; 
   }
-  elseif(mysqli_fetch_array(mysqli_query($con,"SELECT COUNT(*) FROM Events WHERE Name = '" . $_GET['name'] . "' and Universe = '" . $_GET['universe'] . "'"))[0] != 0){
+  elseif(mysqli_fetch_array(mysqli_query($con,"SELECT COUNT(*) FROM Events WHERE Name = '" . $_GET['name'] . "'"))[0] != 0){
     echo 'Sorry, that name is already taken. Please try something slightly more descriptive.';
   }
   elseif(! is_valid_date($_GET['date'])) {
@@ -18,8 +17,8 @@
   }
   else {
     $str = $_GET['name'] . "', '" . $_GET['universe'] . "', '" . $_GET['date'] . "', '" . $_GET['location'] . "')";
-    mysqli_query($con,"INSERT INTO Events (Name, Universe, Date, Location) VALUES ('" . $str);
-    $int = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM Events WHERE Universe = '" . $_GET['universe'] . "' and Name = '" . $_GET['name'] . "'"))['PID'];
+    mysqli_query($con,"INSERT INTO Events (Name, Date, Location) VALUES ('" . $str);
+    $int = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM Events WHERE Name = '" . $_GET['name'] . "'"))['PID'];
     echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=v&id=$int' target='submit'>here</a> to add an extended description.</p>\n";
   }
 ?>
