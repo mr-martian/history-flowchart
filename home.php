64ef96d11d3c5b7a06f7e0bb80636f8663bb1ce3
mr-martian <popcorn.tomato.dude@gmail.com>
2015-01-31 16:24:35 -0600

    Update home.php

--- home.php
+++ home.php
@@ -8,14 +8,14 @@
     <!-- Herein describe this project and its reason for existence. -->
     <br>
     <?php
-      include "settings/general.php";
+      include "globals.php";
       $con = connect();
       echo "<p>When you loaded this page, our database contained ";
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Events"))[0], " events, ";
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Effects"))[0], " cause-effect chains, and ";
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Vessays"))[0] + mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Vessays"))[0], " essays.</p>";
     ?>
-    <a href='submit/submit_event.php' target='submit'>Submit an event</a>
+    <a href='submit/submit_event.html' target='submit'>Submit an event</a>
     <a href='submit/submit_effect_u.php' target='submit'>Submit a cause-and-effect chain</a>
     <table>
       <tr>
