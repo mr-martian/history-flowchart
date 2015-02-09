c3d532c6e6b0687a85b82627c25db4d1ff7c594e
mr-martian <popcorn.tomato.dude@gmail.com>
2015-02-08 20:47:59 -0600

    Update handle_essay.php

--- submit/handle_essay.php
+++ submit/handle_essay.php
@@ -3,14 +3,18 @@
     <?php
       include "../globals.php";
       $con = connect();
-      $w = ($_GET['type'] == 'v' ? 'Vessays' : 'Fessays');
-      mysqli_query($con,"INSERT INTO " . $w . " (Title, About, Name) VALUES ('" . mysqli_real_escape_string($_POST['title']) . "', " . mysqli_real_escape_string($_GET['id']) . "', " . mysqli_real_escape_string($_POST['name']) . ")");
-      $int = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM " . $w . " WHERE Title = " . $_POST['title'] . " and About = " . $_GET['id']))[0]['PID'];
-      $file = fopen("essays/" . ($_GET['type'] == 'v' ? 'events' : 'effects') . "/" . $int . ".txt", "x");
-      if ($file) {
-        fwrite($file, htmlspecialchars($_POST['essay']) . "\n\n");
-        fwrite($file, "<h3>SOURCES</h3>\n" . htmlspecialchars($_POST['sources']));
-        echo "record succesfully added.";
+      $w = ($_POST['type'] == 'v' ? 'Vessays' : 'Fessays');
+      $title = mysqli_real_escape_string($con, $_POST['title']);
+      $id = mysqli_real_escape_string($con, $_POST['id']);
+      $name = mysqli_real_escape_string($con, $_POST['name']);
+      $a = mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM $w WHERE Title = '$title' and About = '$id' and Name = '$name'"));
+      if ($a) { echo "Please try a different title, that one is already taken."; }
+      else {
+        mysqli_query($con, "INSERT INTO $w (Title, About, Name) VALUES ('$title', '$id', '$name');");
+        $a = mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM $w WHERE Title = '$title' and About = '$id' and Name = '$name'"));
+        $int = strval($a['PID']);
+        file_put_contents("../essays/" . ($_POST['type'] == 'v' ? 'events' : 'effects') . "/$int.txt", htmlspecialchars($_POST['essay']) . "\n\n" . "<h3>SOURCES</h3>\n" . htmlspecialchars($_POST['sources']));
+        echo "<h2>record succesfully added.</h2>";
       }
     ?>
   </body>
