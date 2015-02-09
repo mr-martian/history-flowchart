4a80046ca8e9d4a38d12510c2d8757df999b0074
mr-martian <popcorn.tomato.dude@gmail.com>
2015-02-08 20:45:16 -0600

    Update home.php

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     <title>Flowcharthistory.com</title>
   </head>
   <body>
-    <a href='get_graph/show_events.php'>Make a graph</a>
+    <a href='request_graph.php'>Make a graph</a>
     <br>
     <!-- Herein describe this project and its reason for existence. -->
     <br>
@@ -13,14 +13,16 @@
       echo "<p>When you loaded this page, our database contained ";
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Events"))[0], " events, ";
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Effects"))[0], " cause-effect chains, and ";
-      echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Vessays"))[0] + mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Vessays"))[0], " essays.</p>";
+      echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Vessays"))[0] + mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Fessays"))[0], " essays.</p>";
     ?>
-    <a href='submit/submit_event.html' target='submit'>Submit an event</a>
-    <a href='submit/submit_effect_u.php' target='submit'>Submit a cause-and-effect chain</a>
-    <table>
+    <a href='submit/submit_event.html' target='submit'>Submit an event</a> - 
+    <a href='submit/submit_effect_c.php' target='submit'>Submit a cause-and-effect chain</a> - 
+    <a href='get_essay/select_essay.php?m=v' target='submit'>Find an event</a> - 
+    <a href='get_essay/select_essay.php?m=f' target='submit'>Find an effect</a>
+    <table width="100%" height="90%">
       <tr>
-        <td><iframe src="about:blank" name="submit"></iframe></td>
-        <td><iframe src="about:blank" name="result"></iframe></td>
+        <td><iframe src="about:blank" name="submit" width="100%" height="100%"></iframe></td>
+        <td><iframe src="about:blank" name="result" width="100%" height="100%"></iframe></td>
       </tr>
     </table>
   </body>
