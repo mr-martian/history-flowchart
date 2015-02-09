2167116f86166ac6929327c868f5db756d974e18
mr-martian <popcorn.tomato.dude@gmail.com>
2015-02-08 20:49:57 -0600

    Update submit_effect_e.php

--- submit/submit_effect_e.php
+++ submit/submit_effect_e.php
@@ -1,7 +1,4 @@
 <?php
-  include 'settings/general.php';
-  $result = get_event($universe=$_GET['u']);
-  while($row = mysqli_fetch_array($result)) {
-    echo "<br><a href='submit_effect_t.php?u=", $_GET['u'], "&c=", $_GET['c'], "&e=", $row['PID'], "' target='effect'>", $row['Name'], "</a>";
-  }
+  include "../globals.php";
+  event_list("submit_effect_t.php?c=" . $_GET['c'] . "&e=", "");
 ?>
