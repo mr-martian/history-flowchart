1e29e3aa5823125c1c24c8003dd892bb23132ec8
mr-martian <popcorn.tomato.dude@gmail.com>
2015-02-08 20:52:02 -0600

    Update display_essay.php

--- get_essay/display_essay.php
+++ get_essay/display_essay.php
@@ -1,18 +1,12 @@
 <html>
-  <head>
-    <title><?php
-      include "settings/general.php";
-      $con = connect();
-      $es = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM " . ($_GET['type'] == 'v' ? 'V' : 'F') . "essays WHERE PID = " . $_GET['id']));
-      echo $es['Title'];
-    ?></title>
-  </head>
   <body>
     <?php
-      include "settings/general.php";
+      include "../globals.php";
       $con = connect();
-      $es = mysqli_fetch_array(mysqli_query($con,"SELECT * FROM " . ($_GET['type'] == 'v' ? 'V' : 'F') . "essays WHERE PID = " . $_GET['id']));
-      echo "<h1>", $es['Title'] "</h1>";
+      $q = mysqli_query($con,"SELECT * FROM " . ($_GET['type'] == 'v' ? 'V' : 'F') . "essays WHERE PID = " . $_GET['id'] . ';');
+      $es = mysqli_fetch_assoc($q);
+      echo "<h1>" . $es['Title'] . "</h1>";
+      echo "<h3>" . $es['Name'] . "</h3>";
       if ($_GET['type'] == 'v') {
         $p = "events/";
         event_summary($es['About']);
@@ -22,9 +16,9 @@
         effect_summary($es['About']);
       }
       
-      $myfile = fopen("essays/" . $p . $es['PID'] . ".txt", "r") or die("Unable to open file!");
-      echo "<pre>" . fread($myfile,filesize("essays/" . $p . $es['PID'] . ".txt")) . "</pre>";
-      fclose($myfile);
+      echo "<pre>";
+      readfile("../essays/$p" . $es['PID'] . ".txt");
+      echo "</pre>";
     ?>
   </body>
 </html>
