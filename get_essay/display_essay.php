103c764f3180e6262cf0c88edc83759fd9b03e90
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 18:07:08 -0500

    Update display_essay.php

--- get_essay/display_essay.php
+++ get_essay/display_essay.php
@@ -7,14 +7,22 @@
       $es = mysqli_fetch_assoc($q);
       echo "<h1>" . $es['Title'] . "</h1>";
       echo "<h3>" . $es['Name'] . "</h3>";
+      echo "<table border='1'><tr><td colspan='2'>";
       if ($_GET['type'] == 'v') {
         $p = "events/";
         event_summary($es['About']);
+        echo "</td><td><table><tr><th>Essay Tags</th></tr><tr><td>";
+        echo implode(", ", geteventessaytags($_GET['id'])) ?: "[No Tags]";
+        $t = 3;
       }
       else {
         $p = "effects/";
         effect_summary($es['About']);
+        echo "</td><td><table><tr><th>Essay Tags</th></tr><tr><td>";
+        echo implode(", ", geteffectessaytags($_GET['id'])) ?: "[No Tags]";
+        $t = 4;
       }
+      echo "<br><a href='../submit/add_tag.php?id=" . $_GET['id'] . "&type=$t'>Add More</a></td></tr></table></td></tr></table>";
       
       echo "<pre>";
       readfile("../essays/$p" . $es['PID'] . ".txt");
