20827a87fbeae9a72f49ac69aafc04a6fba48f75
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 18:03:00 -0500

    Update handle_effect.php

--- submit/handle_effect.php
+++ submit/handle_effect.php
@@ -4,17 +4,16 @@
   $con = connect();
   $c = mysqli_real_escape_string($con, strval(intval($_GET['c'])));
   $e = mysqli_real_escape_string($con, strval(intval($_GET['e'])));
-  $t = mysqli_real_escape_string($con, $_GET['t']);
   
-  if(mysqli_num_rows(get_effect($cause=$c, $effect=$e, $type=$t)) != 0){
+  if(mysqli_num_rows(get_effect($cause=$c, $effect=$e)) != 0){
     echo 'Sorry, that cause & effect chain has already been entered. Please add something new.';
   }
-  elseif(!is_valid_type($t)) {
-    echo 'Type invalid, please try resubmitting the form.';
+  elseif(get_time_coord(get_event_array($c)) > get_time_coord(get_event_array($e))) {
+    echo 'The cause you have selected occurred after the effect you selected. Our grapher is not equipped to handle time travel, so we are forced to reject your submission.';
   }
   else {
-    if (mysqli_query($con,"INSERT INTO Effects (Cause, Effect, Type) VALUES ('$c', '$e', '$t');")) {
-      $int = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM Effects WHERE Cause='$c' and Effect='$e' and Type='$t'"))['PID'];
+    if (mysqli_query($con,"INSERT INTO Effects (Cause, Effect) VALUES ('$c', '$e');")) {
+      $int = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM Effects WHERE Cause='$c' and Effect='$e';"))['PID'];
       echo "<h1>Record Added Successfully!</h1><br><p>Click <a href='submit_essay.php?type=f&id=$int' target='submit'>here</a> to add an extended description.</p>\n";
     }
     else {
