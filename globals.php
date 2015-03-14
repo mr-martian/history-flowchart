5c272d83d0c70cc71ac9dea2840fbbff785349b2
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 17:55:00 -0500

    Update globals.php

--- globals.php
+++ globals.php
@@ -50,7 +50,7 @@
     mysqli_close($con);
     return $ret;
   }
-  function get_effect($universe=null, $id=null, $cause=null, $effect=null, $type=null) {
+  function get_effect($universe=null, $id=null, $cause=null, $effect=null) {
     $con = connect();
     $str = ' WHERE';
     $added = false;
@@ -64,10 +64,6 @@
     }
     if ($effect) {
       $str .= ($added ? ' AND' : '') . ' Effect = ' . strval(intval($effect));
-      $added = true;
-    }
-    if ($type) {
-      $str .= ($added ? ' AND' : '') . " Location = '" . mysqli_real_escape_string($con, $type) . "'";
       $added = true;
     }
     $ret = mysqli_query($con, "SELECT * FROM Effects" . ($added ? $str : ''));
@@ -89,63 +85,106 @@
   function location_link($str) {
     $ar = sscanf($str, "(%f, %f)");
     return "http://www.openstreetmap.org/?mlat=" . $ar[0] . "&mlon=" . strval(-1 * intval($ar[1])) . "&zoom=6#layers=T";
+  }  
+  function bytag($tag, $type) {
+    $con = connect();
+    $ls = mysqli_query($con, "SELECT * from Tags WHERE Tag = '" . mysqli_real_escape_string($con, $tag) . "' AND Category = '" . strval(intval($type)) . "';");
+    $ret = array();
+    while ($row = mysqli_fetch_assoc($ls)) {
+      array_push($ret, $row["PID"]);
+    }
+    mysqli_close($con);
+    return $ret;
   }
-  function event_summary($eid) {
+  function eventsbytag($tag) { return bytag($tag, 0); }
+  function effectsbytag($tag) { return bytag($tag, 1); }
+  function eventessaysbytag($tag) { return bytag($tag, 2); }
+  function effectessaysbytag($tag) { return bytag($tag, 3); }
+  function gettags($pid, $type) {
+    $con = connect();
+    $ls = mysqli_query($con, "SELECT * from Tags WHERE PID = '" . strval(intval($pid)) . "' AND Category = '" . strval(intval($type)) . "';");
+    $ret = array();
+    while ($row = mysqli_fetch_assoc($ls)) {
+      array_push($ret, $row["Tag"]);
+    }
+    mysqli_close($con);
+    return $ret;
+  }
+  function geteventtags($pid) { return gettags($pid, 0); }
+  function geteffecttags($pid) { return gettags($pid, 1); }
+  function geteventessaytags($pid) { return gettags($pid, 2); }
+  function geteffectessaytags($pid) { return gettags($pid, 3); }
+  function getalltags($type=0) {
+    $con = connect();
+    $ls = mysqli_query($con, "SELECT DISTINCT Tag FROM Tags WHERE Category = '" . strval(intval($type)) . "';");
+    $ret = array();
+    while ($row = mysqli_fetch_assoc($ls)) {
+      array_push($ret, $row["Tag"]);
+    }
+    mysqli_close($con);
+    return $ret;
+  }
+  function settag($pid, $tag, $type) {
+    if (in_array($pid, bytag($tag, $type))) {
+      return true;
+    }
+    else {
+      $con = connect();
+      $type = strval(intval($type));
+      $pid = strval(intval($pid));
+      if (ctype_alnum(str_replace(' ', '', $tag)) and
+          strlen($tag) < 20 and
+          mysqli_query($con,"INSERT INTO Tags (Tag, PID, Category) VALUES ('" . mysqli_real_escape_string($con,$tag) . "', '" . $pid . "', '" . $type . "');")) {
+        mysqli_close($con);
+        return true;
+      }
+      else {
+        mysqli_close($con);
+        return false;
+      }
+    }
+  }
+  function seteventstag($pid, $tag) { return settag($pid, $tag, 0); }
+  function seteffectstag($pid, $tag) { return settag($pid, $tag, 1); }
+  function seteventessaystag($pid, $tag) { return settag($pid, $tag, 2); }
+  function seteffectessaystag($pid, $tag) { return settag($pid, $tag, 3); }
+  function event_summary($eid, $up=false) {
+    $w = $up ? "." : "";
     $ev = get_event_array($eid);
-    echo "<table><tr><th>Name</th><td>", $ev['Name'], "</td></tr>";
+    echo "<table border='1'><tr><th>Name</th><td>", $ev['Name'], "</td></tr>";
     echo "<tr><th>Date</th><td>", $ev['Date'], "</td></tr>";
-    echo "<tr><th>Location</th><td><a target='_blank' href='" . location_link($ev['Location']) . "'>" . $ev['Location'] . "</a></td></tr></table>";
+    echo "<tr><th>Location</th><td><a target='_blank' href='" . location_link($ev['Location']) . "'>" . $ev['Location'] . "</a></td></tr>";
+    echo "<tr><th>Tags</th><td>";
+    echo implode(", ", geteventtags($eid)) ?: "[No Tags]";
+    echo "<br><a href='$w./submit/add_tag.php?id=$eid&type=0'>Add More</a></td></tr></table>";
   }
-  function effect_summary($id) {
+  function effect_summary($id, $up=false) {
+    $w = $up ? "." : "";
     $ef = mysqli_fetch_assoc(get_effect($id=$id));
-    echo "<table border='1'><tr><th>Cause</th><th>Effect</th><th>Type</th></tr><tr><td>";
+    echo "<table border='1'><tr><th>Cause</th><th>Effect</th></tr><tr><td>";
     event_summary($ef['Cause']);
     echo "</td><td>";
     event_summary($ef['Effect']);
-    echo "</td><td>" . $ef['Type'] . "</td></tr></table>";
+    echo "</td></tr><tr><th>Tags</th><td>";
+    $x = geteffecttags($id);
+    if ($x) { echo implode(", ", $x); }
+    else { echo "[No Tags]"; }
+    echo "<br><a href='$w./submit/add_tag.php?id=$id&type=1'>Add More</a>";
+    echo "</td></tr></table>";
   }
-  $types = array('political' => 'red',
-                 'ideological' => 'blue'); //must be less than 15 chars
   function is_valid_date($string) {
     //return preg_match("/^-?\d+(\.\d+)?$/", $string) == 1;
-    return preg_match("/^\d{1,2}\/\d{1,2}\/\d{1,5}$/", $string) == 1;
+    //$match = "/^\d{1,2}\/\d{1,2}\/\d{1,5}$/";
+    $match = "/^(0?\d|1\d)\/(0?\d|(1|2)\d|3(0|1))\/-?\d{1,5}$/";
+    return preg_match($match, $string) == 1;
   }
   function is_valid_place($string) {
     return preg_match("/^\(-?\d+(\.\d+)?, -?\d+(\.\d+)?\)$/", $string) == 1;
   }
-  function is_valid_type($string) {
-    global $types;
-    return array_key_exists($string, $types);
-  }
-  function get_effect_color($effect) {
-    global $types;
-    return $types[$effect['Type']];
-  }
-  function get_event_color($event) {
-    return 'red';
-  }
   function get_space_coord($event) {
     return sscanf($event['Location'], "(%f, %f)")[1];
   }
-  function get_time_coord_old($event) {
-    //return sscanf($event['Date'], "%f")[0] + 5000;
-    //sscanf($event['Date'], "%i/%i/%i", $m, $d, $y);
-    $date = date_parse($event['Date']);
-    $m = $date['month'];
-    $d = $date['day'];
-    $y = $date['year'];
-    $months = array(1 => 0, 2 => 31, 3 => 59, 4 => 90, 5 => 120, 6 => 151, 7 => 181, 8 => 212, 9 => 243, 10 => 273, 11 => 304, 12 => 334, 13 => 365);
-    if ($y % 4 == 0 and ($y % 100 != 0 or $y % 400)) {
-      $months = array(1 => 0, 2 => 31, 3 => 60, 4 => 91, 5 => 121, 6 => 152, 7 => 182, 8 => 213, 9 => 244, 10 => 274, 11 => 305, 12 => 335, 13 => 366);
-    }
-    print_r($m);
-    $yp = $months[$m] + $d - 1;
-    if ($y < 0) {
-      $y++;
-    }
-    return $y + ($yp/$months[13]);
-  }
-  function get_time_coord_2($event) {
+  function get_time_coord($event) {
     list($m, $d, $y) = split('[/]', $event['Date']);
     $months = array(0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365);
     if ($y % 4 == 0 && ($y % 100 != 0 || $y % 400)) {
@@ -154,10 +193,6 @@
     $yp = $months[$m] + $d - 1;
     if ($y < 0) { $y++; }
     return $y + ($yp / $months[13]);
-  }
-  function get_time_coord($event) {
-    sscanf($event['Date'], "%i/%i/%i", $m, $d, $y);
-    return intval($y);
   }
   function event_list($url, $target, $allok=false) {
     $result = get_event();
@@ -237,7 +272,6 @@
       echo "<td><a href='$url" . $row['PID'] . "' target='$target'>" . $row['PID'] . "</a></td>";
       echo "<td>" . $row['Cause'] . "</td>";
       echo "<td>" . $row['Effect'] . "</td>";
-      echo "<td>" . $row['Type'] . "</td>";
       echo "</tr>";
     }
 
