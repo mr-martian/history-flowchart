977b6d2ffeda66f318b51dafc8ab327e66e4b0ea
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 18:07:33 -0500

    Update get_essay.php

--- get_essay/get_essay.php
+++ get_essay/get_essay.php
@@ -2,8 +2,8 @@
   include "../globals.php";
   $con = connect();
   $result = mysqli_query($con,"SELECT * FROM " . ($_GET['type'] == 'v' ? 'V' : 'F') . "essays WHERE About = " . $_GET['id']);
-  if ($_GET['type'] == 'v') {event_summary($_GET['id']);}
-  else {effect_summary($_GET['id']);}
+  if ($_GET['type'] == 'v') {event_summary($_GET['id'], true);}
+  else {effect_summary($_GET['id'], true);}
 
 while($row = mysqli_fetch_assoc($result)) {
   echo "<br><a href='display_essay.php?id=", $row['PID'], "&type=", $_GET['type'], "' target=\"result\">", $row['Title'], "</a>";
