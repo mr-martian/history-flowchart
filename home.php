a14d0849d9c901fe93ea679d6f71e885e055257b
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 17:55:49 -0500

    Update home.php

--- home.php
+++ home.php
@@ -3,10 +3,6 @@
     <title>Flowcharthistory.com</title>
   </head>
   <body>
-    <a href='request_graph.php'>Make a graph</a>
-    <br>
-    <!-- Herein describe this project and its reason for existence. -->
-    <br>
     <?php
       include "globals.php";
       $con = connect();
@@ -15,10 +11,12 @@
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Effects"))[0], " cause-effect chains, and ";
       echo mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Vessays"))[0] + mysqli_fetch_array(mysqli_query($con, "SELECT Count(*) FROM Fessays"))[0], " essays.</p>";
     ?>
+    <a href='request_graph.php' target='_blank'>Make a graph</a>
     <a href='submit/submit_event.html' target='submit'>Submit an event</a> - 
     <a href='submit/submit_effect_c.php' target='submit'>Submit a cause-and-effect chain</a> - 
     <a href='get_essay/select_essay.php?m=v' target='submit'>Find an event</a> - 
     <a href='get_essay/select_essay.php?m=f' target='submit'>Find an effect</a>
+    <a href='' target='_blank'>About</a>
     <table width="100%" height="90%">
       <tr>
         <td><iframe src="about:blank" name="submit" width="100%" height="100%"></iframe></td>
