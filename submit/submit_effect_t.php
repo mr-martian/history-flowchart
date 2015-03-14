c200666a53d821546b5f634f70be30ac933fa98f
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 18:04:36 -0500

    Delete submit_effect_t.php

--- submit/submit_effect_t.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include "../globals.php";
-  foreach (array_keys($types) as $key) {
-    echo "<a href='handle_effect.php?c=", $_GET['c'], "&e=", $_GET['e'], "&t=", $key, "' target='result'>", $key, "</a><br>";
-  }
-?>
