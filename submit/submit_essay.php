8a7411facb7a09b5047936d43bdbfbb2ec3902f1
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 18:05:45 -0500

    Update submit_essay.php

--- submit/submit_essay.php
+++ submit/submit_essay.php
@@ -12,17 +12,17 @@
         effect_summary($_GET['id']);
         $ef = mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM Effects WHERE PID = " . $_GET['id']));
         $title = $ef['Cause'] . " to " . $ef['Effect'];
-        $essay = "the details of how" . $ef['Cause'] . " led to " . $ef['Effect'] . ".";
+        $essay = "the details of how " . $ef['Cause'] . " led to " . $ef['Effect'] . ".";
       }
       
-      echo "<h1>", $title, "</h1>";
+      echo "<h1>$title</h1>";
       echo "<p>Scroll down to submit.</p>";
       echo "<form action='handle_essay.php' method='POST' target='result'>";
       echo "<input type='hidden' name='id' value='" . $_GET['id'] . "'></input><br>";
       echo "<input type='hidden' name='type' value='" . $_GET['type'] . "'></input><br>";
       echo "<input type='text' maxlength='50' name='title'>Title of this Document</input><br>";
       echo "<input type='text' maxlength='30' name='name'>Your Name</input><br>";
-      echo "<textarea rows='20' cols='80' required name='essay'>Please herein describe ", $essay, "</textarea>";
+      echo "<textarea rows='20' cols='80' required name='essay'>Please herein describe $essay</textarea>";
       echo "<textarea rows='15' cols='80' required name='sources'>Sources go here!</textarea>";
       echo "<button type='submit'>Submit</button>";
       echo "</form>";
