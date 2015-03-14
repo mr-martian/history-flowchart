6810a9876aa140b487c3718177d31e59dd8bff24
mr-martian <popcorn.tomato.dude@gmail.com>
2015-03-14 17:56:29 -0500

    Update make_graph.php

--- make_graph.php
+++ make_graph.php
@@ -1,38 +1,5 @@
 <html>
   <head>
-    <script type="text/javascript">
-      function readMouseMove(e){
-        var result = document.getElementById('mousepos');
-        var x = e.clientX - 5000;
-        var y = e.clientY - 180;
-var element = document.getElementById('graph');
-var rX = 0;
-var rY = 0;
-if (element.pageYOffset)
-   {
-   rX=element.pageXOffset;
-   rY=element.pageYOffset;
-   }
-if (document.body)
-   {
-   rX=document.body.scrollLeft;
-   rY=document.body.scrollTop;
-   }
-if (document.documentElement && document.documentElement.scrollTop)
-   {
-   rX=document.documentElement.scrollLeft;
-   rY=document.documentElement.scrollTop;
-   }
-x+=rX;
-y+=rY;
-        if (x < 1) { var xtxt = (1 - x) + " BC"; }
-        else { var xtxt = "AD " + x; }
-        if (y < 0) { var ytxt = (-1 * y) + "&deg;E"; }
-        else { var ytxt = y + "&deg;W"; }
-        result.innerHTML = "Your mouse location: " + xtxt + ", " + ytxt;
-      }
-      document.onmousemove = readMouseMove;
-    </script>
     <style>
       #mousepos {
         position: fixed;
@@ -41,6 +8,20 @@ y+=rY;
         border: solid;
         padding: 3px;
       }
+      #graph {
+        border-width: 0;
+        padding: 0;
+        margin: 0;
+      }
+      div {
+        border-width: 1px;
+        border: solid;
+      }
+      #hide {
+        position: fixed;
+        top: 450px;
+        overflow: hidden;
+      }
     </style>
   </head>
   <body>
@@ -48,27 +29,27 @@ y+=rY;
       include 'globals.php';
 
       function ev2svg($event) {
-        $s0 = '<circle r="5" stroke="black" stroke-width="1" cx="';
-        $s1 = '" cy="';
-        $s2 = '" fill="';
-        $s3 = '" onclick="window.open(\'';
-        $s4 = '\')"><title>';
-        $s5 = '</title></circle>\n';
-        $rs0 = $s0 . get_time_coord($event) . $s1 . get_space_coord($event) . $s2;
-        $rs1 = get_event_color($event) . $s3 . 'get_essay/get_essay.php?type=v&id=' . $event['PID'];
-        return $rs0 . $rs1 . $s4 . $event['Name'] . $s5;
+        $x = get_time_coord($event);
+        $y = get_space_coord($event);
+        $c = implode(" ", geteventtags($event['PID']));
+        $id = $event['PID'];
+        $n = $event['Name'];
+        $s0 = "<circle r='3' stroke='black' stroke-width='1' cx='$x' cy='$y' fill='red' class='";
+        $s1 = "$c' onclick='window.open(\"get_essay/get_essay.php?type=v&id=$id\")'><title>$n</title></circle>\n";
+        return $s0 . $s1;
       }
       function ef2svg($effect) {
         $f = get_event_array($effect['Cause']);
         $t = get_event_array($effect['Effect']);
-        $s0 = '<line x1="' . get_time_coord($f);
-        $s1 = '" y1="' . get_space_coord($f);
-        $s2 = '" x2="' . get_time_coord($t);
-        $s3 = '" y2="' . get_space_coord($t);
-        $s4 = '" stroke-width="3" stroke="' . get_effect_color($effect);
-        $s5 = '" onclick="window.open(\'' . 'get_essay/get_essay.php?type=f&id=' . $effect['PID'];
-        $s6 = '\')" marker-end="url(#triangle)"><title>' . $f['Name'] . ' to ' . $t['Name'];
-        return $s0 . $s1 . $s2 . $s3 . $s4 . $s5 . $s6 . '</title></line>\n';
+        $fx = get_time_coord($f);
+        $fy = get_space_coord($f);
+        $tx = get_time_coord($t);
+        $ty = get_space_coord($t);
+        $c = implode(" ", geteventtags($effect['Cause'])) . " " . implode(" ", geteventtags($effect['Effect']));
+        $s0 = "<line x1='$fx' y1='$fy' x2='$tx' y2='$ty' stroke-width='2' stroke='blue' class='$c'";
+        $s1 = " onclick='window.open(\"get_essay/get_essay.php?type=f&id=" . $effect['PID'];
+        $s2 = "\")' marker-end='url(#triangle)'><title>" . $f['Name'] . ' to ' . $t['Name'];
+        return $s0 . $s1 . $s2 . '</title></line>\n';
       }
       function graph_causes($event) {
         $ls = get_effects_by_effect($event['PID']);
@@ -119,7 +100,7 @@ y+=rY;
         }
       }
       echo '<svg id="graph" height="390" width="7100" zoomAndPan="magnify">';
-      echo '<marker xmlns="http://www.w3.org/2000/svg" id="triangle" viewBox="0 0 10 10" refX="10" refY="5" markerUnits="strokeWidth" markerWidth="4" markerHeight="3" orient="auto">
+      echo '<marker xmlns="http://www.w3.org/2000/svg" id="triangle" viewBox="0 0 10 10" refX="8" refY="5" markerUnits="strokeWidth" markerWidth="4" markerHeight="3" orient="auto">
       <path d="M 0 0 L 10 5 L 0 10 z"/></marker><g transform="translate(5000, 180)">\n';
       graph();
       echo '</g>';
@@ -132,7 +113,15 @@ y+=rY;
         echo "</text>\n";
       }
       echo '</svg>';
+      echo '<p id="mousepos">0,0</p>';
+      $x = getalltags();
+      echo "<div id='hide'><h4>Hide events with tags:</h4>";
+      foreach ($x as $i) {
+        echo "<input type='checkbox' value='$i'>$i</input>\n";
+      }
+      echo "<br><button id='update' onclick='dochecks();'>Update</button></div>";
     ?>
-  <p id="mousepos">0,0</p>
+  <script type="text/javascript" src="graph_mouse.js"></script>
+  <script type="text/javascript" src="graph_filter.js"></script>
   </body>
 </html>
